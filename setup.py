9fdece9c69f5324325e2e71e10aadf2b44d85ed2
Joel <joel3000@gmail.com>
2017-07-07 11:17:56 -0700

    setup.py / ansible debugging.

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from distutils.core import setup
 setup(
     name='commute',
     version='0.1dev',
-    packages=['src',],
-    install_requires=['googlemaps==2.5.1',
-                      'peewee==2.10.0']
+    packages=['src',]
 )
