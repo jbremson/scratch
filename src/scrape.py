7d40fc7b01a791061ef982d150dad1a7855bda19
Joel <joel3000@gmail.com>
2017-07-07 15:58:18 -0700

    Added cron.

--- src/scrape.py
+++ src/scrape.py
@@ -1,7 +1,8 @@
 import argparse
 from src.loader import Cities, Trips
 
-parser = argparse.ArgumentParser()
+
+parser = argparse.ArgumentParser(description="Run the scraper that collects drive time data.")
 parser.add_argument("-n", "--no-write", help="Do not write to the database, i.e. test run.", action="store_false")
 parser.add_argument("-c", "--load-cities", help="Load new cities", action="store_false")
 parser.add_argument("-x", "--no-scrape", help="Do not run the scraper", action="store_true")
@@ -11,3 +12,4 @@ if args.load_cities:
     Cities.load_cities()
 if not args.no_scrape:
     Trips.process_trips(no_write=args.no_write);
+print("Done")
