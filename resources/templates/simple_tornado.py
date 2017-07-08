a1e8710a2a58f889bde2932b1f483df584e2c10c
Joel <joel3000@gmail.com>
2017-07-08 00:31:27 -0700

    Added simple_tornado.py to templates.

---
+++ resources/templates/simple_tornado.py
@@ -0,0 +1,16 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8111, address='192.168.33.10')
+    tornado.ioloop.IOLoop.current().start()
